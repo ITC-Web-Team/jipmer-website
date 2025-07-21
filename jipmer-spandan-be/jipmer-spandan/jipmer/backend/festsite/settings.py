7037d091b7f0b9a5de1173985165505345d91ef5
JIPMER2025 <niranjanapnair157@gmail.com>
2025-07-22 00:45:56 +0530

    Update settings.py

--- jipmer-spandan-be/jipmer-spandan/jipmer/backend/festsite/settings.py
+++ jipmer-spandan-be/jipmer-spandan/jipmer/backend/festsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('django-insecure-#ja(l7s3@4)o@hb#h&9oksbarjo05jkp65h9u&d3#$pm$xh!el')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
@@ -84,6 +84,7 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
+  "https://www.jipmerspandan.in",
   "http://localhost:5173",
 ]
 
