d981e2c29cb30f575e5b1a3b66a44a98ff37d440
JIPMER2025 <niranjanapnair157@gmail.com>
2025-07-22 01:11:44 +0530

    Update email.py

--- jipmer-spandan-be/jipmer-spandan/jipmer/backend/registration/utils/email.py
+++ jipmer-spandan-be/jipmer-spandan/jipmer/backend/registration/utils/email.py
@@ -6,6 +6,6 @@ def send_smtp_email(to_email, subject, message):
         subject,
         message,
         os.getenv("DEFAULT_FROM_EMAIL"),  # From
-        [to_email],                       # To
+        [to_email],                      # To
         fail_silently=False,
     )
